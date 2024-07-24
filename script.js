85fc9fad392203fd2712007f2b733cf52e6889f4
Jude Kiron Mensah <kjmensah11@gmail.com>
2024-07-24 10:47:31 +0000

    Added the Account and Cart pages

--- script.js
+++ script.js
@@ -34,3 +34,37 @@ smallimg[2].onclick = function(){
 smallimg[3].onclick = function(){
     MainImg.src = smallimg[3].src;
 }
+
+
+// JavaScript for Cart toggle menu
+var MenuItems = document.getElementById("MenuItems");
+
+        MenuItems.style.maxHeight = "0px";
+
+        function menutoggle(){
+           if(MenuItems.style.maxHeight == "0px")
+                {
+                    MenuItems.style.maxHeight = "200px";
+                } 
+            else {
+                MenuItems.style.maxHeight = "0px";
+            }
+        }
+
+
+// JavaScript for the Account page forms
+var LoginForm = document.getElementById("LoginForm");
+        var RegisterForm = document.getElementById("RegisterForm");
+        var Indicator = document.getElementById("Indicator");
+
+            function register(){
+                RegisterForm.style.transform = "translateX(0px)";
+                LoginForm.style.transform = "translateX(0px)";
+                Indicator.style.transform = "translateX(100px)";
+            }
+
+            function login(){
+                RegisterForm.style.transform = "translateX(300px)";
+                LoginForm.style.transform = "translateX(300px)";
+                Indicator.style.transform = "translateX(0px)";
+            }
